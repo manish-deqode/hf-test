84281d1311e641aff9098d09ca1be85360a0512a
manish-deqode <76510457+manish-deqode@users.noreply.github.com>
2021-12-15 18:42:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $array = ['niklesh', 'manish'];
+echo "set - 1001";
 if(mail('nraut@deqode.com, mthakur@deqode.com', 'array data', '<pre>'.print_r($array, true).'</pre>')){
 echo "true";
 }else{
