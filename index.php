38938aa34cd750351c1d32882ba3e8f8b37233de
manish-deqode <76510457+manish-deqode@users.noreply.github.com>
2021-12-15 18:39:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 $array = ['niklesh', 'manish'];
-mail('email@domain.com', 'array data', '<pre>'.print_r($array, true).'</pre>');
+if(mail('email@domain.com', 'array data', '<pre>'.print_r($array, true).'</pre>')){
+echo "true";
+}else{
+echo "false";
+}
 ?>
